e06062f78ee2d36a6e2fda2bdec5ffee5452fbe1
Rojek240784 <krzysztof-rojek7@wp.pl>
2022-01-11 23:49:04 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/untitle3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitle3.dir/main.cpp.obj"
+  "libuntitle3.dll.a"
+  "untitle3.exe"
+  "untitle3.exe.manifest"
+  "untitle3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitle3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
